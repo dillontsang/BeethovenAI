0f81ddb836363d841c756843a4f29c1af10ab0be
Dillon Tsang <93083409+dillontsang@users.noreply.github.com>
2023-09-02 14:20:41 -0400

    Add files via upload

--- generateharmony.py
+++ generateharmony.py
@@ -204,10 +204,9 @@ def chord_to_midi(degree, quality, inversion, secondary_dominant_numeral):
         # deal with secondary dominant
         if secondary_dominant_numeral != "" and secondary_dominant_numeral != 0:
             degree = degree + int(secondary_dominant_numeral) - 1
-            if degree > 8:
-                degree -= 8
-            elif degree == 8:
-                degree = 1
+            if degree >= 8:
+                degree -= 7
+                
                 
         # Major, Minor, Diminished, Augmented, 
         # Dominant, Major 7th, Minor 7th, Half Diminished, Fully Diminished
